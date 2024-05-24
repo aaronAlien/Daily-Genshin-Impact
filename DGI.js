9233d80a66a5a984ad6e082ba1069830c1f6520d
Aaron <thatshakespeare2@gmail.com>
2024-05-24 17:19:12 +0100

    both fetch requests successful - style changes, new font, new colors - unfinished

--- DGI.js
+++ DGI.js
@@ -8,7 +8,7 @@ const week = [
   "Thursday",
   "Friday",
   "Saturday",
-  "Sunday"
+  "Sunday",
 ];
 
 const dayDisplay = document.getElementById("day-display");
@@ -25,26 +25,22 @@ const currentDateString = currentDate.toLocaleDateString("en-UK", {
 // weekday output
 dayDisplay.textContent = `${week[dayIndex]}`;
 
+
 fetch("https://genshin.jmp.blue/materials/talent-book")
   .then((res) => res.json())
   .then((data) => {
-    //console.log(data);
+   // console.log(data);
     // console.log(Array.isArray(data));
 
     // Convert the object's values into an array
     const dataArray = Object.values(data);
-    console.log(dataArray);
+    //console.log(dataArray);
 
     dataArray.forEach((book) => {
-      const currentDayBooks = document.getElementById("currentDayBooks");
+      let currentDayBooks = document.getElementById("currentDayBooks");
       if (book.availability.includes(week[dayIndex])) {
-        currentDayBooks.innerHTML += `
-      <h2>${book.items[0].name}</h2>
-      <p>${book.characters.join(', ')}</p>
-    `;
+        currentDayBooks.innerHTML += `<h4>${book.items[0].name}</h4> <p>${book.characters.join(' | ')}</p>`;
       }
-  
-
     });
 
     if (!Array.isArray(data)) {
@@ -53,4 +49,48 @@ fetch("https://genshin.jmp.blue/materials/talent-book")
   })
   .catch((error) => console.log(error));
 
+  fetch("https://genshin.jmp.blue/materials/weapon-ascension")
+  .then((res) => res.json())
+  .then((data) => {
+    //console.log(data);
 
+    const dataArray2 = Object.values(data);
+    //console.log(dataArray2);
+
+    dataArray2.forEach((weapon) => {
+      let currentDayWeapons = document.getElementById('currentDayWeapons');
+      if (weapon.availability.includes(week[dayIndex])) {
+        currentDayWeapons.innerHTML += `<p>${weapon.items[0].name}</p>`;
+      }
+    });
+    if (!Array.isArray(data)) {
+      return false;
+    }
+  })
+  .catch((error) => console.log(error));
+
+  
+// links function
+const openLink = (url) => {
+  window.open(url, "_blank");
+};
+
+const mapBtn = document
+  .getElementById("mapBtn")
+  .addEventListener("click", () =>
+    openLink("https://act.hoyolab.com/ys/app/interactive-map/index.html")
+  );
+
+const checkInBtn = document
+  .getElementById("checkInBtn")
+  .addEventListener("click", () =>
+    openLink(
+      "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481"
+    )
+  );
+
+const wikiBtn = document
+  .getElementById("wikiBtn")
+  .addEventListener("click", () =>
+    openLink("https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki")
+  );
