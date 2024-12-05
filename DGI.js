4d5390c8091c982f73e1357b5043010150516da9
Aaron <thatshakespeare2@gmail.com>
2024-12-05 00:02:24 +0000

    cool

--- DGI.js
+++ DGI.js
@@ -1,3 +1,50 @@
+/* NAV */
+const hamburger = document.getElementById("hamburger-btn")
+
+hamburger.addEventListener("click", function () {
+  const navMenu = document.querySelector(".links ul");
+  navMenu.classList.toggle("active");
+});
+
+
+/* MODAL */
+const modalContainer = document.getElementById("modal-container");
+const modal = document.getElementById("modal");
+
+const openModal = () => {
+  modalContainer.style.display = "flex";
+
+  // scroll
+modal.scrollIntoView({
+  behavior: 'smooth', // Smooth scrolling
+  block: 'center',    // Scroll to the center of the modal
+});
+};
+
+
+const closeModal = () => {
+  modalContainer.style.display = "none";
+
+  //clear search
+  mySearch.value = "";
+
+  const imgContainer = document.querySelector(".char-img-container");
+  if (imgContainer) {
+    imgContainer.remove();
+  }
+};
+
+const closeButton = document
+  .getElementById("close-btn")
+  .addEventListener("click", closeModal);
+
+//close click outside
+window.onclick = (e) => {
+  if (e.target === modalContainer) {
+    closeModal();
+  }
+};
+
 // weekdays
 const week = [
   "Sunday",
@@ -22,10 +69,13 @@ function h5Style() {
   const h5Titles = document.querySelectorAll("h5");
 
   h5Titles.forEach((h5) => {
+    h5.style.textAlign = "center";
+    h5.style.maxWidth = "80%";
+    h5.style.margin = "0 auto";
     h5.style.fontSize = "1.3rem";
     h5.style.marginBottom = ".8rem";
     h5.style.color = "var(--color-a)";
-    h5.style.borderBottom = "2px solid rgba(105, 111, 165, 0.906)";
+    h5.style.borderBottom = "2px solid rgba(144,85,207, 0.9)";
   });
 }
 
@@ -91,13 +141,19 @@ fetch("https://genshin.jmp.blue/materials/weapon-ascension")
     dataArray2.forEach((mat) => {
       if (mat.availability?.includes(week[dayIndex])) {
         let weaponDiv = document.createElement("div");
-        let todaysWeaponMats = mat.items[0].name;
+
+        let todaysWeaponMats = mat.items[3].id;
+        //let todaysWeaponMats = mat.items[0].name;
+        console.log('Weapon materials: ', todaysWeaponMats)
+
         let title2 = document.createElement("h5");
         title2.textContent = todaysWeaponMats;
+
         weaponDiv.appendChild(title2);
 
         let todaysWeapons = mat.weapons;
-
+        console.log('Todays weapons: ', todaysWeapons)
+        
         todaysWeapons.forEach((weapon) => {
           //console.log(weapon);
 
@@ -110,10 +166,10 @@ fetch("https://genshin.jmp.blue/materials/weapon-ascension")
           imageElement.alt = `${weapon}`;
 
           weaponDiv.appendChild(imageElement);
-        });
+        });;
         currentDayWeapons.appendChild(weaponDiv);
-      }
-    });
+    }
+  });
     h5Style();
 
     if (!Array.isArray(data)) {
@@ -152,19 +208,21 @@ async function fetchData() {
     // search output - text
     const searchOutput = document.getElementById("searchOutput");
     searchOutput.innerHTML = `
-    ${dataArray3[0]}<br/>
-    "${dataArray3[1]}"<br/>
-    Nation: ${dataArray3[5]}<br/>
-    Vision: ${dataArray3[2]}<br/>
-    Weapon: ${dataArray3[3]}<br/>
-    Rarity: ${dataArray3[7]}⭐<br/>
-    Description: ${dataArray3[11]}`;
 
-    searchOutput.style.maxWidth = "100%";
-    searchOutput.style.borderRadius = "1rem";
-    searchOutput.style.paddingTop = "1rem";
-    searchOutput.style.backgroundColor = "rgba(224, 224, 224, 0.2)";
-    searchOutput.style.color = "var(--color-a)";
+    <div class="modal-name">
+      <h3>${dataArray3[0]}</h3>
+      <p>"${dataArray3[1]}"</p>
+    </div>
+
+    <ul>
+  <li>Nation: ${dataArray3[5]}</li>
+  <li>Vision: ${dataArray3[2]}</li>
+  <li>Weapon: ${dataArray3[3]}</li>
+  <li>Rarity: ${dataArray3[7]}⭐</li>
+  <li>Description: ${dataArray3[11]}</li>
+</ul>
+    
+    `;
 
     // search output - image
     const imgContainer = document.createElement("div");
@@ -178,13 +236,77 @@ async function fetchData() {
     img.src = url;
 
     imgContainer.appendChild(img);
-    searchOutput.append(imgContainer);
+    modal.append(imgContainer);
   } catch (error) {
     console.log(error);
   }
-  mySearch.value = "";
 }
+mySearch.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    fetchData();
+    openModal();
+  }
+});
 
 const searchBtn = document
   .getElementById("searchBtn")
-  .addEventListener("click", fetchData);
+  .addEventListener("click", () => {
+    fetchData();
+    openModal();
+  });
+
+/* wish banner */
+const wishOne = "../assets/wishBanner/5Point2_Phase1_char1.webp";
+const wishTwo = "../assets/wishBanner/5Point2_Phase1_char2.webp";
+const wishWeapon = "../assets/wishBanner/5Point2_Phase1_weapon.webp";
+
+class WishBanner {
+  constructor() {
+    this.slides = [wishOne, wishTwo, wishWeapon];
+    this.slideInfo = [
+      `Boosted Drop Rate for "Skyborne Arbiter" Chasca (Anemo)!`,
+      `Boosted Drop Rate for "Spectacle of Phantasmagoria" Lyney (Pyro)`,
+      `Boosted Drop Rate for Astral Vulture's Crimson Plumage (Bow) and The First Great Magic (Bow)!`,
+    ];
+
+    this.currentIndex = 0;
+    this.carouselInterval = null;
+
+    this.render();
+    this.startCarousel();
+  }
+
+  render() {
+    const container = document.getElementById("wish-banner");
+    container.innerHTML = `
+    <div class="carousel-container">
+    
+    <img
+    src='${this.slides[this.currentIndex]}'
+    alt='ADD ALT'
+    class='carousel-image' />
+    <span>${this.slideInfo[this.currentIndex]}</span>
+    <span>Until 2024/12/10</span>
+    </div>
+    `;
+  }
+
+  startCarousel() {
+    this.carouselInterval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+      this.render();
+    }, 3000);
+  }
+
+  stopCarousel() {
+    if (this.carouselInterval) {
+      clearInterval(this.carouselInterval);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const wishBanner = new WishBanner();
+});
+
